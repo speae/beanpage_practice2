bd8c8e168141106222ed37ffc7e8bbe1e4af5a60
이익수 <speae@beanpage.com>
2020-10-12 07:43:30 +0900

    연습용 게시판 - 이익수 - 게시판 페이지 기본 구조 구성 https://trello.com/c/h2JKwwwz

---
+++ back/write/write.js
@@ -0,0 +1,23 @@
+/**
+ * /board 랜더링 준비 처리
+ * @param request
+ * @param response
+ * @param render_data
+ * @param {[]} query_list
+ * @return {Promise<boolean>}
+ */
+module.exports = async function(request, response, render_data, query_list) {
+    const COMMON = require('../class/common');
+    try{
+
+        render_data.source = "write/write";
+        throw "SUCCESS";
+    }catch (e) {
+        if (e === "SUCCESS") {
+            return true;
+        } else {
+            render_data.err_msg = e;
+            return false;
+        }
+    }
+};
