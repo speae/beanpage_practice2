9bad50d36f5dbee3fc4a7d445e14811244860b9e
이익수 <speae@beanpage.com>
2020-10-15 08:23:35 +0900

    연습용 게시판 - 이익수 - 게시글 삭제 기능 구현 중 https://trello.com/c/h2JKwwwz

---
+++ back/board/delete.js
@@ -0,0 +1,40 @@
+/**
+ *
+ * @param request
+ * @param response
+ * @param render_data
+ * @param query_list
+ * @returns {Promise<boolean>}
+ */
+module.exports = async function(request, response, render_data, query_list) {
+    const COMMON = require('../class/common');
+    const DAO_MYSQL = require('../class/dao_mysql');
+
+    const db = new DAO_MYSQL();
+
+    try {
+        if (request.query.num === undefined) {
+            throw "글번호를 찾을 수 없습니다.";
+        }
+        const num = request.query.num;
+
+        const sql = "SELECT * FROM board_table WHERE num=?";
+        const viewData = await db.queryOne(sql, [num]);
+
+        if (viewData === false) {
+            throw "개인 게시글 불러오기 실패.";
+        }
+
+        render_data.title = "게시판";
+        render_data.source = "board/delete";
+        render_data.rows = viewData;
+        throw "SUCCESS";
+    }catch (e) {
+        if (e === "SUCCESS") {
+            return true;
+        } else {
+            render_data.err_msg = e;
+            return false;
+        }
+    }
+};
