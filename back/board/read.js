42543eec0537eceb58881a0c35385cc32724b064
이익수 <speae@beanpage.com>
2020-10-14 14:38:30 +0900

    연습용 게시판 - 이익수 - 개인이 작성한 게시글 불러오기 작업중 https://trello.com/c/h2JKwwwz

--- back/board/read.js
+++ back/board/read.js
@@ -8,36 +8,31 @@
  */
 module.exports = async function(request, response, render_data, query_list) {
     const COMMON = require('../class/common');
-
-    const jsdom = require("jsdom");
-    const {JSDOM} = jsdom;
-    const {window} = new JSDOM();
-    const {document} = (new JSDOM('')).window;
-    global = document;
-    let jQuery = require('jquery')(window);
-    const $ = jQuery;
-
     const DAO_MYSQL = require('../class/dao_mysql');
     const db = new DAO_MYSQL();
-    const num = $(location).attr('search').slice($(location).attr('search').indexOf('=')+1);
-    console.log(num);
 
-    const sql = "SELECT * FROM board_table WHERE num=" + num;
-    const selectData = await db.query(sql, []);
-    if (selectData === false) {
+    if(request.query.num === false){
+        throw "글번호를 찾을 수 없습니다.";
+    }
+    const num = request.query.num;
+    const selectView = num;
+    const sql = "SELECT * FROM board_table WHERE num="+selectView;
+    const viewData = await db.query(sql, []);
+    if (viewData === false) {
         throw "개인 게시글 불러오기 실패.";
     }
-    try{
-        render_data.title = "게시판";
-        render_data.source = "board/read?num="+num;
-        render_data.rows = selectData;
-        throw console.log("개인 게시글 불러오기.");
-    }catch (e) {
-        if (e === "SUCCESS") {
-            return true;
-        } else {
-            render_data.err_msg = e;
-            return false;
+    try {
+            render_data.title = "게시판";
+            render_data.source = "board/read";
+            render_data.rows = viewData;
+            throw console.log("개인 게시글 불러오기.");
+        } catch (e) {
+            if (e === "SUCCESS") {
+                return true;
+            } else {
+                render_data.err_msg = e;
+                return false;
+            }
         }
-    }
+
 };
