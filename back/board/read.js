a4d2d40ccdd756ae1651e672440dd958e03ce17d
이익수 <speae@beanpage.com>
2020-10-14 17:28:43 +0900

    연습용 게시판 - 이익수 - 개인이 작성한 게시글 불러오기 추가, 조회수 증가 수정중 https://trello.com/c/h2JKwwwz

--- back/board/read.js
+++ back/board/read.js
@@ -6,33 +6,51 @@
  * @param query_list
  * @returns {Promise<boolean>}
  */
-module.exports = async function(request, response, render_data, query_list) {
+module.exports = async function (request, response, render_data, query_list) {
     const COMMON = require('../class/common');
     const DAO_MYSQL = require('../class/dao_mysql');
+
     const db = new DAO_MYSQL();
 
-    if(request.query.num === false){
-        throw "글번호를 찾을 수 없습니다.";
-    }
-    const num = request.query.num;
-    const selectView = num;
-    const sql = "SELECT * FROM board_table WHERE num="+selectView;
-    const viewData = await db.query(sql, []);
-    if (viewData === false) {
-        throw "개인 게시글 불러오기 실패.";
-    }
     try {
-            render_data.title = "게시판";
-            render_data.source = "board/read";
-            render_data.rows = viewData;
-            throw console.log("개인 게시글 불러오기.");
-        } catch (e) {
-            if (e === "SUCCESS") {
-                return true;
-            } else {
-                render_data.err_msg = e;
-                return false;
-            }
+        if (request.query.num === undefined) {
+            throw "글번호를 찾을 수 없습니다.";
+        }
+        const num = request.query.num;
+
+        const sql = "SELECT * FROM board_table WHERE num=?";
+        const viewData = await db.queryOne(sql, [num]);
+
+        if (viewData === false) {
+            throw "개인 게시글 불러오기 실패.";
         }
 
+        const countSql = "SELECT count FROM board_table WHERE num=?";
+        const count_data = await db.queryOne(countSql, [num]);
+        if (count_data === false) {
+            throw "조회 실패";
+        }
+        const upCount = parseInt(count_data['count']) + 1;
+
+        db.setTable('board_table');
+        db.add('count', upCount);
+
+        if (await db.update(num, [upCount.toString()]) === false) {
+            throw "조회 실패.";
+        }
+
+        render_data.title = "게시판";
+        render_data.source = "board/read";
+        render_data.rows = viewData;
+
+        throw "SUCCESS";
+    } catch (e) {
+        if (e === "SUCCESS") {
+
+            return true;
+        } else {
+            render_data.err_msg = e;
+            return false;
+        }
+    }
 };
