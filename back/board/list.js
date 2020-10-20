0cb32b14fdcd2e089333cc3a187ff267318b897d
이익수 <speae@beanpage.com>
2020-10-20 13:24:38 +0900

    연습용 게시판 - 이익수 -게시글 페이징 기능 구현 중 https://trello.com/c/h2JKwwwz

--- back/board/list.js
+++ back/board/list.js
@@ -13,15 +13,35 @@ module.exports = async function(request, response, render_data, query_list) {
     const db = new DAO_MYSQL();
 
     const sql="SELECT * FROM board_table ORDER BY num DESC";
-
     const resultData = await db.query(sql, []);
     if(resultData===false){
         throw "게시글 불러오기 실패.";
     }
+
+
+    const pageSize = 10;
+    const pageBlock = 10;
+    const count = "SELECT COUNT(*) FROM board_table"
+    const boardCount = await db.queryOne(count, []);
+
+    if(boardCount===false){
+        throw "게시글 불러오기 실패.";
+    }
+
+    const countResult = parseInt(boardCount['COUNT(*)']);
+    const currentPage = COMMON.toInt(request.query.page);
+
     try{
+
+        render_data.boardCount = countResult;
+        render_data.pageSize = pageSize;
+        render_data.pageBlock = pageBlock;
+        render_data.currentPage = currentPage;
+
         render_data.title = "게시판";
         render_data.source = "board/list";
         render_data.rows = resultData;
+
         throw "SUCCESS";
     }catch (e) {
         if (e === "SUCCESS") {
