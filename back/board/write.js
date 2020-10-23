2462c7d6df78fce14f67bcd27850366c0eddab98
이익수 <speae@beanpage.com>
2020-10-23 09:05:43 +0900

    연습용 게시판 - 이익수 -본문 작성이나 수정 시 해당 본문 복귀 설정 https://trello.com/c/h2JKwwwz

--- back/board/write.js
+++ back/board/write.js
@@ -8,8 +8,21 @@
  */
 module.exports = async function(request, response, render_data, query_list) {
     const COMMON = require('../class/common');
+    const DAO_MYSQL = require('../class/dao_mysql');
+
+    const db = new DAO_MYSQL();
+
     try{
 
+        const sql = "SELECT MAX(num) FROM board_table";
+        const numData = await db.queryOne(sql, []);
+        const number = parseInt(numData['MAX(num)']);
+
+        if(numData === false){
+            throw "게시글 번호 불러오기 실패";
+        }
+
+        render_data.number = number;
         render_data.title = "게시판";
         render_data.source = "board/write";
         throw "SUCCESS";
